084d688940703d2d4b33f8387072b7a4db46849f
mike632t <1799044+mike632t@users.noreply.github.com>
2026-09-14 00:35:47 +0100

    Use George MARSAGLIA's xorshift algorithm

--- rand.h
+++ rand.h
@@ -28,18 +28,81 @@
 #define RAND_H
 #define RAND_MAX 32767
 
-int xs_seed = 1; // Initial value for seed
+unsigned int xs_seed = 1; /* Initial value for seed */
 
 /**
  * @fn     int rand(void)
- * @brief  Generate a pseudo-random value between 0 and RAND_MAX (both included).
+ * @brief  Generate a 16-bit pseudo-random integer value.
  * @return value
+ * 
+ * Generates  a psudo  random  number  using  George  MARSAGLIA's  xorshift 
+ * algorithm.	This  generates a 16-bit pseudo-random number  by  applying 
+ * three  successive  xorshift	 operations, where  the	 current  value	 is
+ * exclusive-ORed with a shifted copy of itself.
+ * 
+ * This  version is implemented in C using _unsigned_ 16 bit  integers.  In
+ * terms  of performance it is less than 10% slower then the version  below 
+ * using assembler which shows how efficient the compiler is.
  */
 rand()
 {
-	// return (xs_seed = ((xs_seed * 3) + 1) & 0x7FFF);
+      xs_seed ^= xs_seed << 7;
+      xs_seed ^= xs_seed >> 9;
+      xs_seed ^= xs_seed << 8;
+      return xs_seed;
+}
 
-        return (xs_seed = (((xs_seed << 1) + xs_seed) + 1));
+/**
+ * @fn     int rand(void)
+ * @brief  Generate a 16-bit pseudo-random integer value.
+ * @return value
+ * 
+ * Generates  a psudo  random  number  using  George  MARSAGLIA's  xorshift 
+ * algorithm.	This  generates a 16-bit pseudo-random number  by  applying 
+ * three  successive  xorshift	 operations, where  the	 current  value	 is
+ * exclusive-ORed with a shifted copy of itself.
+ * 
+ * This version was implemented in Z80 assembly by John METCALF. 
+ * 
+ * http://www.retroprogramming.com/2017/07/xorshift-pseudorandom-numbers-in-z80.html
+ * 
+ */
+rand2()
+{
+#asm
+;   unsigned xorshift(n)
+;   {
+;       n ^= n << 7;
+;       n ^= n >> 9;
+;       n ^= n << 8;
+;       return n;
+;   }
+;
+; Implemented using assembler.
+;
+;		POP	DE		; Pop the 16-bit Return Address off the stack into DE.
+;		EX	(SP),HL		; Get current seed from the stack
+;		PUSH	DE		; Put the Return Address back on the stack.
+
+		LD	HL, (xs_seed)	; Get current seed...
+		LD	A,H	
+		RRA
+		LD	A,L
+		RRA
+		XOR	H
+		LD	H,A
+		LD	A,L
+		RRA
+		LD	A,H
+		RRA
+		XOR	L
+		LD	L,A
+		XOR	H
+		LD	H,A
+		LD 	(xs_seed), HL	; Save seed.
+		RET
+;
+#endasm
 }
 
 /**
